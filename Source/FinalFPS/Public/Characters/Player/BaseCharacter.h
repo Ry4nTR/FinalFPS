818986d12202b17d9403f2293e9d949e101be2d0
Ry4nTR <rostukterletskyy@gmail.com>
2025-06-29 20:20:33 +0200

    Initial commit
    Add initial project files and setup.

--- Source/FinalFPS/Public/Characters/Player/BaseCharacter.h
+++ Source/FinalFPS/Public/Characters/Player/BaseCharacter.h
@@ -5,37 +5,8 @@
 #include "InputActionValue.h"
 #include "BaseCharacter.generated.h"
 
-class UCameraComponent;
-class UInputMappingContext;
-class UInputAction;
-
 UCLASS()
 class FINALFPS_API ABaseCharacter : public ACharacter
 {
     GENERATED_BODY()
-
-public:
-    ABaseCharacter();
-
-protected:
-    virtual void BeginPlay() override;
-    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-    // Input callbacks
-    void Move(const FInputActionValue& Value);
-    void LookAround(const FInputActionValue& Value);
-
-protected:
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-    UCameraComponent* CameraComponent;
-
-    // Input
-    UPROPERTY(EditDefaultsOnly, Category = "Input")
-    UInputMappingContext* InputMapping;
-
-    UPROPERTY(EditDefaultsOnly, Category = "Input")
-    UInputAction* MoveAction;
-
-    UPROPERTY(EditDefaultsOnly, Category = "Input")
-    UInputAction* LookAction;
 };
